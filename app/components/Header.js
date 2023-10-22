6bb5c306008a0fb595618690c227e455a7b077d3
Emmanuel Norambuena <eanorambuena@uc.cl>
2023-10-22 15:57:30 -0300

    Fix bg in navbar

--- app/components/Header.js
+++ app/components/Header.js
@@ -16,7 +16,7 @@ function Header () {
   }, [hidden]);
 
   return /*html*/`
-    <nav class="bg-white border-gray-200 bg-gray-900">
+    <nav class="border-gray-200 bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://emmyjs.github.io/" class="flex items-center">
           <img src="/logo.png" alt="Emmy.js logo" class="h-8 mr-3">
@@ -29,9 +29,9 @@ function Header () {
           </svg>
         </button>
         <div class="hidden w-50 md:block md:w-auto" id="navbar-default" aria-expanded="false">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
+          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
             <li>
-              <a href="/" onclick="route(event)" class="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 text-white md:text-blue-500" aria-current="page">Home</a>
+              <a href="/" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent" aria-current="page">Home</a>
             </li>
             <li>
               <a href="/about" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">About</a>
