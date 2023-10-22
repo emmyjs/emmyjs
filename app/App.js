287d8e3ed31cb8cf98254a0742e78c454ff3a9de
Emmanuel Norambuena <eanorambuena@uc.cl>
2023-10-22 15:44:42 -0300

    Add flowbite navbar

--- app/App.js
+++ app/App.js
@@ -1,20 +1,17 @@
 import { load } from "../emmy.js";
 
 import "./Home.js";
+import "./components/Header.js";
 load('/About.html', 'About');
 load('/Contact.html', 'Contact');
 load('/Code404.html', 'Code404');
 
 function App () {
-    this.behave('div');
-    this.className = 'flex flex-col justify-space-between space-y-3 text-center w-full h-full text-white';
+  this.behave('div');
+  this.className = 'flex flex-col justify-space-between space-y-3 text-center w-full h-full text-white';
 
   return /*html*/`
-    <nav>
-      <a href="/" onclick="route(event)">Home</a>
-      <a href="/about" onclick="route(event)">About</a>
-      <a href="/contact" onclick="route(event)">Contact</a>
-    </nav>
+    <Header></Header>
     <Route href="/" to="Home"></Route>
     <Route href="/about" to="About"></Route>
     <Route href="/404" to="Code404"></Route>
