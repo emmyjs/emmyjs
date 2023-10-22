56c773385c32e2b87e6d46f47a2fa95699c4cc44
Emmanuel Norambuena <eanorambuena@uc.cl>
2023-10-22 17:05:52 -0300

    Add UnderConstruction

--- app/App.js
+++ app/App.js
@@ -1,19 +1,23 @@
 import { load } from "../emmy.js";
 
 import "./Home.js";
+import "./Docs.js";
+import "./Contact.js";
 import "./components/Header.js";
-load('/About.html', 'About');
-load('/Contact.html', 'Contact');
 load('/Code404.html', 'Code404');
 
 function App () {
   this.behave('div');
-  this.className = 'flex flex-col justify-space-between space-y-3 text-center w-full h-full text-white';
+  this.className = 'flex flex-col justify-space-between space-y-3 text-center w-full h-full text-white box-border';
+
+  this.callback = () => {
+    this.querySelector('Router').className += ' box-border';
+  }
 
   return /*html*/`
     <Header></Header>
     <Route href="/" to="Home"></Route>
-    <Route href="/about" to="About"></Route>
+    <Route href="/docs" to="Docs"></Route>
     <Route href="/404" to="Code404"></Route>
     <Route href="/contact" to="Contact"></Route>
     <Router></Router> 
--- app/components/Header.js
+++ app/components/Header.js
@@ -16,7 +16,7 @@ function Header () {
   }, [hidden]);
 
   return /*html*/`
-    <nav class="border-gray-200 bg-gray-900">
+    <nav class="border-gray-200 bg-gray-900 z-40">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://emmyjs.github.io/" class="flex items-center">
           <img src="/logo.png" alt="Emmy.js logo" class="h-8 mr-3">
@@ -34,7 +34,7 @@ function Header () {
               <a href="/" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent" aria-current="page">Home</a>
             </li>
             <li>
-              <a href="/about" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">About</a>
+              <a href="/docs" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">Docs</a>
             </li>
             <li>
               <a href="/contact" onclick="route(event)" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">Contact</a>
