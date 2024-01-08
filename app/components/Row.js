5d663e2c583471446a75b305ac000d96f6e95bf7
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-01-07 21:38:11 -0300

    Update styling and add legend to status component

--- app/components/Row.js
+++ app/components/Row.js
@@ -3,7 +3,7 @@ import "./Pill.js";
 
 export function row() {
   return html`
-    <li class="py-3 sm:py-4 justify-between space-x-3 rtl:space-x-reverse flex items-center">
+    <li class="py-3 sm:py-4 justify-between space-x-3 rtl:space-x-reverse flex items-center gap-5">
       <p class="text-sm font-semibold text-gray-900 truncate dark:text-white">
         ${this.innerHTML}
       </p>
