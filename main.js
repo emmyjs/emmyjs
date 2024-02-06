50ba27cc7d1836170b42b12e148401277ab8b01d
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-02-06 19:47:04 -0300

    Update meta description and component headings

--- main.js
+++ main.js
@@ -12,12 +12,20 @@ import { status } from './app/Status.js'
 import { marked } from 'marked'
 import { readFileSync } from 'node:fs'
 
-const md = readFileSync('./public/1.0.0.md', 'utf-8')
+const renderMarkdown = async (text) => {
+  const htmlText = await marked(text)
+  return `<section class="flex flex-col justify-top items-left text-left p-4 pt-0 mb-4 sm:w-[90%] md:w-[70%] h-fit box-border gap-6">${htmlText}</section>`
+    .replace(/&#39;/g, "'")
+    .replace(/`/g, '\\`')
+    .replace(/\${/g, '\\${')
+    .replace(/<h1>/g, '<h1 class="text-3xl md:text-5xl font-extrabold mb-2 text-purple-600 dark:text-purple-300">')
+    .replace(/<h2>/g, '<h2 class="text-3xl font-extrabold mb-2 text-emerald-600 dark:text-emerald-300">')
+    .replace(/<h3>/g, '<h3 class="text-2xl font-bold mb-2 text-blue-600 dark:text-blue-300">')
+    .replace(/<h4>/g, '<h4 class="text-xl font-bold mb-2 text-indigo-600 dark:text-indigo-300">')
+}
 
-let result = `<section class="flex flex-col justify-top items-left text-left p-4 pt-0 mb-4 sm:w-[90%] md:w-[70%] h-fit box-border gap-6">${await marked(md)}</section>`
-  .replace(/&#39;/g, "'").replace(/`/g, '\\`').replace(/\${/g, '\\${')
-  .replace(/<h1>/g, '<h1 class="text-3xl md:text-5xl font-extrabold mb-2 text-purple-600 dark:text-purple-300">')
-  .replace(/<h2>/g, '<h2 class="text-3xl font-extrabold mb-2 text-emerald-600 dark:text-emerald-300">')
+const md = readFileSync('./public/1.0.0.md', 'utf-8')
+let result = await renderMarkdown(md)
 
 build({
   app: App,
