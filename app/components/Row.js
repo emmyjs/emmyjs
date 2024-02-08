a475a5b91b2ef16dfb6dbc00daecff90c7b8adf4
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-02-08 12:31:02 -0300

    Refactor row function to use props parameter

--- app/components/Row.js
+++ app/components/Row.js
@@ -1,13 +1,13 @@
 import { load, html } from 'emmy-dom/dist/server.js'
 import './Pill.js'
 
-export function row() {
+export function row({ props }) {
   return html`
     <li class='py-3 sm:py-4 justify-between space-x-3 rtl:space-x-reverse flex items-center gap-5'>
       <p class='text-sm font-semibold text-gray-900 truncate dark:text-white'>
         ${this.innerHTML}
       </p>
-      <Pill type='${this.getAttribute('status')}'></Pill>
+      <Pill type='${props().status()}'></Pill>
     </li>
   `
 }
