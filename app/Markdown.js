57996cff4f218f3b97aa4414595562f90488a6bd
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-02-08 21:40:51 -0300

    fix(Root): Fix width of mardown pages on mobile

--- app/Markdown.js
+++ app/Markdown.js
@@ -1,28 +1,28 @@
 import { load } from 'emmy-dom/dist/server.js'
 
 export function index () {
-  this.className = 'flex flex-col justify-center items-center text-center w-full h-fit box-border'
+  this.className = 'flex flex-col justify-center items-center text-center max-w-[90%] lg:max-w-full h-fit box-border'
   return Emmy.markdown.index
 }
 
 load(index, 'Index')
 
 export function rails () {
-  this.className = 'flex flex-col justify-center items-center text-center w-full h-fit box-border'
+  this.className = 'flex flex-col justify-center items-center text-center max-w-[90%] lg:max-w-full h-fit box-border'
   return Emmy.markdown.rails
 }
 
 load(rails, 'Rails')
 
 export function vite () {
-  this.className = 'flex flex-col justify-center items-center text-center w-full h-fit box-border'
+  this.className = 'flex flex-col justify-center items-center text-center max-w-[90%] lg:max-w-full h-fit box-border'
   return Emmy.markdown.vite
 }
 
 load(vite, 'Vite')
 
 export function cli () {
-  this.className = 'flex flex-col justify-center items-center text-center w-full h-fit box-border'
+  this.className = 'flex flex-col justify-center items-center text-center max-w-[90%] lg:max-w-full h-fit box-border'
   return Emmy.markdown.cli
 }
 
--- emmydocs.js
+++ emmydocs.js
@@ -4,7 +4,7 @@ import { readFileSync } from 'node:fs'
 
 export const renderMarkdown = async (text) => {
   const htmlText = await marked(text)
-  return html`<section class="flex flex-col justify-top items-left text-left p-4 pt-0 mb-4 sm:w-[90%] md:w-[70%] h-fit box-border gap-6">${htmlText}</section>`
+  return html`<section class="flex flex-col justify-top items-left text-left p-4 pt-0 mb-4 w-full sm:w-[90%] md:w-[70%] h-fit box-border gap-6">${htmlText}</section>`
     .replace(/`/g, '\`')
     .replace(/\${/g, '\${')
     .replace(/<a/g, '<a class="underline text-blue-600 dark:text-blue-300 hover:text-blue-800 dark:hover:text-blue-400 " ')
